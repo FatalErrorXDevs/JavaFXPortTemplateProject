660088f90f19f7d26f86e975d3a2fad588e298d7
Tom Prins <fatalerrorxdevs@gmail.com>
2018-02-26 09:16:58 +0100

    init

---
+++ GluonMultiView/GluonMultiViewApp/src/main/java/com/gluonmultiview/views/PrimaryPresenter.java
@@ -0,0 +1,39 @@
+package com.gluonmultiview.views;
+
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import com.gluonmultiview.GluonMultiView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PrimaryPresenter {
+
+    @FXML
+    private View primary;
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        primary.showingProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue) {
+                AppBar appBar = MobileApplication.getInstance().getAppBar();
+                appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> 
+                        MobileApplication.getInstance().showLayer(GluonMultiView.MENU_LAYER)));
+                appBar.setTitleText("Primary");
+                appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> 
+                        System.out.println("Search")));
+            }
+        });
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+}
